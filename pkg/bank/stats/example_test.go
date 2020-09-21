cd16b083794c9267358657eceddf044768018b5d
UMuhitdinov <usmon1983@gmail.com>
2020-09-21 15:38:06 +0500

    исправлен функционал

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -8,22 +8,24 @@ import (
 func ExampleAvg()  {
 	payments := []types.Payment {
 		{
+			ID: 1,
 			Amount: 10_000_00,
 			Category: "Рестораны",
 		},
 		{
+			ID: 2,
 			Amount: 20_000_00,
 			Category: "Рестораны",
 		},
 		{
+			ID: 3,
 			Amount: 30_000_00,
 			Category: "Рестораны",
 		},
 	}
 	PaymentAvg := Avg(payments)
 
-	for _, payment := range PaymentAvg {
-		fmt.Println(payment)
+	fmt.Println(PaymentAvg)
 	}
 
 	//Output: 20_000_00
--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -6,5 +6,13 @@ import (
 
 //Avg рассчитывает среднюю сумму платежа
 func Avg(payments []types.Payment) types.Money {
-	
+	//payments_in :=  []types.Payment{}
+	sum := 0
+	next := 0
+	for _, payment := range payments {
+		sum += int(payment.Amount)
+		next += 1
+	}
+	AVG := sum/next
+	return types.Money(AVG)
 }
